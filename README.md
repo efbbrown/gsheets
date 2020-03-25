d2017ca1edd31fdb151c6acfb39c875879ee4a68
Eugene Brown <efbbrown@gmail.com>
2020-03-25 09:23:47 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interacting with Google Sheets using Python
+
+This package contains functions to make downloading and uploading files to and from google sheets a breeze.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gsheets-efbbrown",
+    version="0.0.1",
+    author="Eugene Brown",
+    author_email="efbbrown@gmail.com",
+    description="A package to download from and upload to google sheets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/efbbrown/gsheets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
